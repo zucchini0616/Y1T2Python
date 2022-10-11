94d06121661771b10ebbbf6b5bc209799d826cdf
zhiheng <khorzhiheng@gmail.com>
2022-10-11 17:25:47 +0800

    merged with chart

---
+++ frontend/OctCategory.py
@@ -0,0 +1,35 @@
+import csv
+import pandas as pd
+import os
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+
+db = pd.read_csv("data\Oct19.csv")
+
+def category(db):
+    df = pd.DataFrame(db)
+
+    cat = df['category_code']
+    accessories = cat.str.contains('accessories')
+    apparel = cat.str.contains('apparel')
+
+    #df['category_code'] = np.where(accessories, 'accessories',
+            #                             np.where(apparel, 'apparel',
+                        #                        cat.str.replace('.', ' ')))
+                                                
+    #df['category_code'].value_counts()[-1:-5].plot(kind='bar')
+    df['category_code'] = df['category_code'].str.split('.').str[0]
+    trythis = df['category_code'].value_counts().nlargest(5)
+    st.bar_chart(trythis)
+# .plot(kind='bar')
+    # plt.title("Top 5 category of October 2019")
+    # plt.xlabel("Category")
+    # plt.ylabel("Number of purchases")
+    # plt.show()
+
+#print(a)
+#plt.hist(a)
+#plt.show()
+# category(db)
--- frontend/chart.py
+++ frontend/chart.py
@@ -3,21 +3,24 @@ import time
 import numpy as np
 import pandas as pd
 from dailysales import dailysalesjan,dailysalesOct
+from OctCategory import category
+
 def chart1():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+    category()
+    # progress_bar = st.sidebar.progress(0)
+    # status_text = st.sidebar.empty()
+    # last_rows = np.random.randn(1, 1)
+    # chart = st.line_chart(last_rows)
 
-    for i in range(1, 50):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+    # for i in range(1, 50):
+    #     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    #     status_text.text("%i%% Complete" % i)
+    #     chart.add_rows(new_rows)
+    #     progress_bar.progress(i)
+    #     last_rows = new_rows
+    #     time.sleep(0.05)
 
-    progress_bar.empty()
+    # progress_bar.empty()
 
 def chart2():
     data = dailysalesjan()    
@@ -25,3 +28,5 @@ def chart2():
 def chart3():
     data = dailysalesOct()    
     st.line_chart(data)
+
+
