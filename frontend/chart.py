da8c6750fde21523f16fef866df75813f3bccf80
zhiheng <khorzhiheng@gmail.com>
2022-10-11 18:40:37 +0800

    hi

--- frontend/chart.py
+++ frontend/chart.py
@@ -5,24 +5,9 @@ import pandas as pd
 from dailysales import dailysalesjan,dailysalesOct
 from OctCategory import category
 
-def chart1():
-    print(option)
-    db = pd.read_csv("data\Oct19.csv")
-    category(db)
-    # progress_bar = st.sidebar.progress(0)
-    # status_text = st.sidebar.empty()
-    # last_rows = np.random.randn(1, 1)
-    # chart = st.line_chart(last_rows)
-
-    # for i in range(1, 50):
-    #     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    #     status_text.text("%i%% Complete" % i)
-    #     chart.add_rows(new_rows)
-    #     progress_bar.progress(i)
-    #     last_rows = new_rows
-    #     time.sleep(0.05)
-
-    # progress_bar.empty()
+# def chart1():
+#     db = pd.read_csv("data\Oct19.csv")
+#     category(db)
 
 def chart2():
     data = dailysalesjan()    
