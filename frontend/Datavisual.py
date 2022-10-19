5439fea801833a627ce71a3a8b6269747038598c
HEZEXI <2b14.6070.hezexi@gmail.com>
2022-10-19 14:34:57 +0800

    complete chart page

--- frontend/Datavisual.py
+++ frontend/Datavisual.py
@@ -1,4 +1,3 @@
-from chart import chart2,chart3
 import streamlit as st
 from OctCategory import category,shing1,jav1
 import pandas as pd
@@ -9,85 +8,190 @@ from collections import Counter
 from collections import OrderedDict
 from TopFiveBrands_Month import   calvin1
 from PriceCategorization import calvin2
+from chart import  chart2
 def chartframe():
     option = st.selectbox('Please select the month you want to view',('Apr 2020', 'Mar 2020', 'Feb 2020', 'Jan 2020',
         'Dec 2019', 'Nov 2019', 'Oct 2019'))
-    if option == "Jan 2020":
-            col1, col2= st.columns(2)
-            with col1:
-                st.header("Number of Daily sale of January 2020")
-                chart2()
-            with col2:
-                st.header("Top 5 category of January 2020")
-                db = pd.read_csv("data\Jan20.csv")               
-                category(db)
-    elif option == "Oct 2019":
+    if option == "Apr 2020":
             col1, col2= st.columns(2)
             col3, col4= st.columns(2)
-            col5, col6 = st.columns(2)
+            col5, col6= st.columns(2)
+            db = pd.read_csv("data\Apr20.csv")
             with col1:
-                st.header("Number of Daily sale of October 2019")
-                chart3()
-            with col2:
-                st.header("Top 5 category of October 2019")
-                db = pd.read_csv("data/Oct19.csv")         
-                #df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))     
+                st.header("Top 5 category of April 2020 by no. of purchase")             
                 category(db)
-            with col3:
-                st.header("Sales amount from Oct 19 to Apr 20")
-                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
-            #db = pd.read_csv("data\Oct19.csv")              
-                shing1(df)
-            with col4:
-                st.header("Top 5 category of October 2019 per day")
-                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
-                shing2(df)
-            with col5:
-                st.header("Top 5 category of October 2019 per day")
-
-    elif option == "Nov 2019":
-            col1, col2= st.columns(2)
-            with col1:
-                st.header("Sales amount from Oct 19 to Apr 20")
-                df = pd.read_csv('data/Feb20.csv')
-                # db = pd.rea
             with col2:
-                st.header("Sales amount from Oct 19 to Apr 20")
+                st.header("Top 5 category of April 2020 by sales amount")
+                jav1(db)
+            with col3:
+                st.header("Top 5 brand of April 2020 by no. of purchase")
+                calvin1(db)
+            with col4:
+                st.header("Price Range of April")
                 df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
-                # db = pd.read_csv("data\Oct19.csv")              
                 calvin2(df)
+            with col5:
+                st.header("No. of purchase trend in April")
+                chart2(db)
+            with col6:
+                st.header("Sales amount over from Oct 2019 to Apr 2020")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+                shing1(df)
+
+    elif option == "Mar 2020":
+            col1, col2= st.columns(2)
+            col3, col4= st.columns(2)
+            col5, col6= st.columns(2)
+            db = pd.read_csv("data\Mar20.csv")
+            with col1:
+                st.header("Top 5 category of March 2020 by no. of purchase")             
+                category(db)
+            with col2:
+                st.header("Top 5 category of March 2020 by sales amount")
+                jav1(db)
+            with col3:
+                st.header("Top 5 brand of March 2020 by no. of purchase")
+                calvin1(db)
+            with col4:
+                st.header("Price Range of March")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+                calvin2(df)
+            with col5:
+                st.header("No. of purchase trend in March")
+                chart2(db)
+            with col6:
+                st.header("Sales amount over from Oct 2019 to March 2020")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+                shing1(df)
+
 
     elif option == "Feb 2020":
             col1, col2= st.columns(2)
+            col3, col4= st.columns(2)
+            col5, col6= st.columns(2)
+            db = pd.read_csv("data\Feb20.csv")
             with col1:
-                st.header("Sales amount from Oct 19 to Apr 20")
-                df = pd.read_csv('data/Feb20.csv')
-                # db = pd.read_csv("data\Oct19.csv")              
-                calvin1(df)
+                st.header("Top 5 category of Feburary 2020 by no. of purchase")             
+                category(db)
             with col2:
-                st.header("Sales amount from Oct 19 to Apr 20")
-                df = pd.read_csv('data/Mar20.csv')
-                # db = pd.read_csv("data\Oct19.csv")              
-                jav1(df)
-          
-            
-                # db = pd.read_csv("data\Oct19.csv")              
-                # shing2(db)
-                
-        # col1, col2= st.columns(2)
-        # # col3, col4 = st.columns(2)
+                st.header("Top 5 category of Feburary 2020 by sales amount")
+                jav1(db)
+            with col3:
+                st.header("Top 5 brand of Feburary 2020 by no. of purchase")
+                calvin1(db)
+            with col4:
+                st.header("Price Range of Feburary")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+                calvin2(df)
+            with col5:
+                st.header("No. of purchase trend in Feburary")
+                chart2(db)
+            with col6:
+                st.header("Sales amount over from Oct 2019 to Apr 2020")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+                shing1(df)
 
-        # with col1:    
-        #         chart1()
+    elif option == "Jan 2020":
+            col1, col2= st.columns(2)
+            col3, col4= st.columns(2)
+            col5, col6= st.columns(2)
+            db = pd.read_csv("data\Jan20.csv")
+            with col1:
+                st.header("Top 5 category of January 2020 by no. of purchase")             
+                category(db)
+            with col2:
+                st.header("Top 5 category of January 2020 by sales amount")
+                jav1(db)
+            with col3:
+                st.header("Top 5 brand of January 2020 by no. of purchase")
+                calvin1(db)
+            with col4:
+                st.header("Price Range of January")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+                calvin2(df)
+            with col5:
+                st.header("No. of purchase trend in January")
+                chart2(db)
+            with col6:
+                st.header("Sales amount over from Oct 2019 to Apr 2020")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+                shing1(df)
 
-        # with col2:
+    elif option == "Dec 2019":
+            col1, col2= st.columns(2)
+            col3, col4= st.columns(2)
+            col5, col6= st.columns(2)
+            db = pd.read_csv("data\Dec20.csv")
+            with col1:
+                st.header("Top 5 category of December 2020 by no. of purchase")             
+                category(db)
+            with col2:
+                st.header("Top 5 category of December 2020 by sales amount")
+                jav1(db)
+            with col3:
+                st.header("Top 5 brand of December 2020 by no. of purchase")
+                calvin1(db)
+            with col4:
+                st.header("Price Range of December")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+                calvin2(df)
+            with col5:
+                st.header("No. of purchase trend in December")
+                chart2(db)
+            with col6:
+                st.header("Sales amount over from Oct 2019 to Apr 2020")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+                shing1(df)
 
+    elif option == "Nov 2019":
+            col1, col2= st.columns(2)
+            col3, col4= st.columns(2)
+            col5, col6= st.columns(2)
+            db = pd.read_csv("data\Nov20.csv")
+            with col1:
+                st.header("Top 5 category of November 2020 by no. of purchase")             
+                category(db)
+            with col2:
+                st.header("Top 5 category of November 2020 by sales amount")
+                jav1(db)
+            with col3:
+                st.header("Top 5 brand of November 2020 by no. of purchase")
+                calvin1(db)
+            with col4:
+                st.header("Price Range of November")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+                calvin2(df)
+            with col5:
+                st.header("No. of purchase trend in November")
+                chart2(db)
+            with col6:
+                st.header("Sales amount over from Oct 2019 to Apr 2020")
+                df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+                shing1(df)
 
-    # with col3:
-    #     st.header("chart 3")
-    #     chart3()
-        
-    # with col4:
-    #     st.header("chart 4")
-        
+    elif option == "Oct 2019":
+        col1, col2= st.columns(2)
+        col3, col4= st.columns(2)
+        col5, col6= st.columns(2)
+        db = pd.read_csv("data\Oct20.csv")
+        with col1:
+            st.header("Top 5 category of October 2020 by no. of purchase")             
+            category(db)
+        with col2:
+            st.header("Top 5 category of October 2020 by sales amount")
+            jav1(db)
+        with col3:
+            st.header("Top 5 brand of October 2020 by no. of purchase")
+            calvin1(db)
+        with col4:
+            st.header("Price Range of October")
+            df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+            calvin2(df)
+        with col5:
+            st.header("No. of purchase trend in October")
+            chart2(db)
+        with col6:
+            st.header("Sales amount over from Oct 2019 to Apr 2020")
+            df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))   
+            shing1(df)
         
--- frontend/chart.py
+++ frontend/chart.py
@@ -2,18 +2,19 @@ import streamlit as st
 import time
 import numpy as np
 import pandas as pd
-from dailysales import dailysalesjan,dailysalesOct
-from OctCategory import category,shing1
-
 # def chart1():
 #     db = pd.read_csv("data\Oct19.csv")
 #     category(db)
 
-def chart2():
-    data = dailysalesjan()    
-    st.line_chart(data)
-def chart3():
-    data = dailysalesOct()    
-    st.line_chart(data)
+def chart2(df):
+
+    
+    df['event_time'] = df['event_time'].astype('string')
+    
+    df['event_time'] = df['event_time'].str.split(r" ", expand=True)[0]
+    df = df['event_time'].value_counts()
+       
+    st.line_chart(df)
+
 
 
